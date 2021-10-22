5ca1b5a38c2ae0fb1845715cbd2d474262b196c6
Sven A. Schmidt <sas@finestructure.co>
2021-10-22 06:08:23 +0200

    Add ArgumentParser dependency

--- Package.swift
+++ Package.swift
@@ -5,12 +5,16 @@ import PackageDescription
 let package = Package(
     name: "spi-benchmark",
     dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMajor(from: "0.5.0")),
         .package(url: "https://github.com/pointfreeco/swift-parsing.git", .upToNextMajor(from: "0.1.2"))
     ],
     targets: [
         .executableTarget(
             name: "spi-benchmark",
-            dependencies: [.product(name: "Parsing", package: "swift-parsing")]),
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Parsing", package: "swift-parsing")
+            ]),
         .testTarget(
             name: "spi-benchmarkTests",
             dependencies: ["spi-benchmark"]),
--- Sources/spi-benchmark/main.swift
+++ Sources/spi-benchmark/main.swift
@@ -1,1 +1,17 @@
-print("Hello, world!")
+import ArgumentParser
+
+
+struct Repeat: ParsableCommand {
+    @Option(name: .shortAndLong, help: "Number of test iterations.")
+    var count = 1
+
+    mutating func run() throws {
+        for i in 0..<count {
+            print("\(i)\t testing...")
+        }
+    }
+}
+
+
+Repeat.main()
+
