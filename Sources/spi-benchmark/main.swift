bbce992e8d42da58df58b9557757a4c0bc747450
Sven A. Schmidt <sas@finestructure.co>
2021-10-22 06:38:49 +0200

    Add --work-dir

--- Sources/spi-benchmark/main.swift
+++ Sources/spi-benchmark/main.swift
@@ -6,10 +6,13 @@ struct Repeat: ParsableCommand {
     @Option(name: .shortAndLong, help: "Number of test iterations.")
     var count = 1
 
+    @Option(name: .shortAndLong, help: "Working directory where the test is run.")
+    var workDir = "."
+
     mutating func run() throws {
         for i in 0..<count {
             print("\(i)\t testing...")
-            let res = try ShellOut.shellOut(to: "ls -l")
+            let res = try ShellOut.shellOut(to: "ls -l", at: workDir)
             print(res)
         }
     }
